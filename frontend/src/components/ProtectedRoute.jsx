94b3553c0310dc218a5fc73cd0a00fe43804cc59
sonalidevloper <sjena6401@gmail.com>
2026-03-14 12:37:56 +0530

    Added protectRoute.jsx in components

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const ProtectedRoute = ({ children, adminOnly = false }) => {
+  const { user, token } = useSelector((state) => state.auth);
+
+  if (!token || !user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (adminOnly && user.role !== 'admin') {
+    return <Navigate to="/home" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
