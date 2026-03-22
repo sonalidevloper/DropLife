f62a904ae0a199e1b65d97db2455aac027b90164
sonalidevloper <sjena6401@gmail.com>
2026-03-23 03:09:36 +0530

    Modified landing page

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import SplitTextAnimation from '../components/SplitTextAnimation';
 import './Welcome.css';
 
 const Welcome = () => {
   const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate('/home');
+      // If user is logged in, go to home, otherwise go to login
+      if (token) {
+        navigate('/home');
+      } else {
+        navigate('/login');
+      }
     }, 3500);
 
     return () => clearTimeout(timer);
-  }, [navigate]);
+  }, [navigate, token]);
 
   return (
     <div className="welcome-page">
       <Container className="welcome-container">
         <div className="welcome-content">
           <SplitTextAnimation 
-            text="Welcome to DROPLIFE" 
+            text="Welcome to DR🩸PLIFE" 
             className="welcome-title"
           />
           <div className="welcome-subtitle fade-in">
